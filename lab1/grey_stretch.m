1e6384df91c3463a35a1684372061c8d6a60f94f
unknown <>
2016-11-25 21:35:38 +0800

    Fix lab1

--- lab1/grey_stretch.m
+++ lab1/grey_stretch.m
@@ -4,6 +4,12 @@ function grey = grey_stretch(file)
 [pic, map] = read_img(file);   % get grey
 if isempty(map)
     grey = rgb2grey(pic);
+    column = 5;
+    set (gcf,'Position',[100, 100, 1200, 500]);
+else
+    grey = pic;
+    column = 3;
+    set (gcf,'Position',[300, 100, 800, 500]);
 end
 % caculate the best arguments
 maxi = max(max(grey));
@@ -19,31 +25,13 @@ a = str2double(answer(1));
 b = str2double(answer(2));
 
 if ~isnan(a) && ~isnan(b)
-    set (gcf,'Position',[100, 100, 1200, 500]);
-    
     figure(1);
-    subplot(2, 5, 1);
-    imshow(pic, map);
-    title('Original picture');
-    
-    hsi = rgb2hsi(pic);
-    i  = floor(hsi(:,:,3) .* 255 + 0.5);
-    figure(1);
-    subplot(2, 5, 6);
-    PR = get_probability(i);  % caculate each grey level probability
-    plot(0:255, PR, 'Color',[0.9 0.8 0]);    % draw as hitogram
-    xlim([0 255]);               % limit the x of the hitogram
-    title('Original intensity');
-    xlabel('Intensity level * 255');
-    ylabel('Times');
-    
-    figure(1);
-    subplot(2, 5, 2);
+    subplot(2, column, 1);
     imshow(grey);
     title('Original grey');
 
     figure(1);
-    subplot(2, 5, 7);
+    subplot(2, column, column + 1);
     PR = get_probability(grey);  % caculate each grey level probability
     plot(0:255, PR, 'black');    % draw as hitogram
     xlim([0 255]);               % limit the x of the hitogram
@@ -53,12 +41,12 @@ if ~isnan(a) && ~isnan(b)
     
     stre_grey = grey .* a + b;        % gery stretching
     figure(1);
-    subplot(2, 5, 3);
+    subplot(2, column, 2);
     imshow(stre_grey);
     title('Grey stretch');
     
     figure(1);
-    subplot(2, 5, 8);
+    subplot(2, column, column + 2);
     PR = get_probability(stre_grey);  % caculate each grey level probability
     plot(0:255, PR, 'black');    % draw as hitogram
     xlim([0 255]);               % limit the x of the hitogram
@@ -68,12 +56,12 @@ if ~isnan(a) && ~isnan(b)
     
     equa_grey = grey_equalize(grey);
     figure(1);
-    subplot(2, 5, 4);
+    subplot(2, column, 3);
     imshow(equa_grey);
     title('Grey equalize');
     
     figure(1);
-    subplot(2, 5, 9);
+    subplot(2, column, column + 3);
     PR = get_probability(equa_grey);  % caculate each grey level probability
     plot(0:255, PR, 'black');    % draw as hitogram
     xlim([0 255]);               % limit the x of the hitogram
@@ -81,24 +69,42 @@ if ~isnan(a) && ~isnan(b)
     xlabel('Grey level');
     ylabel('Times');
     
-    h = hsi(:,:,1);
-    s = hsi(:,:,2);
-    i = grey_equalize(i);
-    hsi = cat(3, h, s, i / 255);
-    pic = hsi2rgb(hsi);
-    figure(1);
-    subplot(2, 5, 5);
-    imshow(pic, map);
-    title('Picture equalize');
+    if isempty(map)
+        figure(1);
+        subplot(2, column, 4);
+        imshow(pic, map);
+        title('Original picture');
+
+        hsi = rgb2hsi(pic);
+        i  = floor(hsi(:,:,3) .* 255 + 0.5);
+        figure(1);
+        subplot(2, column, column + 4);
+        PR = get_probability(i);  % caculate each grey level probability
+        plot(0:255, PR, 'Color',[0.9 0.8 0]);    % draw as hitogram
+        xlim([0 255]);               % limit the x of the hitogram
+        title('Original intensity');
+        xlabel('Intensity level * 255');
+        ylabel('Times');
     
-    figure(1);
-    subplot(2, 5, 10);
-    PR = get_probability(i);  % caculate each grey level probability
-    plot(0:255, PR, 'Color',[0.9 0.8 0]);    % draw as hitogram
-    xlim([0 255]);               % limit the x of the hitogram
-    title('Intensity equalize');
-    xlabel('Intensity level * 255');
-    ylabel('Times');
+        h = hsi(:,:,1);
+        s = hsi(:,:,2);
+        i = grey_equalize(i);
+        hsi = cat(3, h, s, i / 255);
+        pic = hsi2rgb(hsi);
+        figure(1);
+        subplot(2, 5, 5);
+        imshow(pic, map);
+        title('Picture equalize');
+
+        figure(1);
+        subplot(2, 5, 10);
+        PR = get_probability(i);  % caculate each grey level probability
+        plot(0:255, PR, 'Color',[0.9 0.8 0]);    % draw as hitogram
+        xlim([0 255]);               % limit the x of the hitogram
+        title('Intensity equalize');
+        xlabel('Intensity level * 255');
+        ylabel('Times');
+    end
 else
     error('Input error!');
 end
--- rgb2grey.m
+++
@@ -1,8 +0,0 @@
-function grey = rgb2grey(rgb)
-% input rgb, return grey
-
-r = rgb(:, :, 1);
-g = rgb(:, :, 2);
-b = rgb(:, :, 3);
-% caculate
-grey = 0.29900 * r +  0.58700 * g + 0.11400 * b;
