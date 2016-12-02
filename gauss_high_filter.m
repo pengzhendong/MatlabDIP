ec4f7b5f66ae18b90dfa66e544ad2e2294d7e529
pengzhendong <275331498@qq.com>
2016-12-03 00:04:45 +0800

    Finish filter

---
+++ gauss_high_filter.m
@@ -0,0 +1,51 @@
+function gauss_high_filter(file, d)
+% input the file name and a distance, show the picture after high filter
+
+[img, map] = imread(file);
+if isempty(map)
+    img = rgb2gray(img);
+end
+% Original picture
+subplot(231), imshow(img);
+title('Original picture');
+
+% Flourier transformation && Frequency shift
+s = fftshift(fft2(img));
+subplot(232), imshow(abs(s), []);
+title('Flourier & Shift');
+
+% Get log
+subplot(233), imshow(log(abs(s)), []);
+title('Getlog');
+
+
+% Get file info
+[m, n] = size(s);
+h = zeros(m, n);
+x = round(m/2);
+y = round(n/2);
+
+for u = 1:m
+    for v = 1:n
+        distance = sqrt((u - x)^2 + (v - y)^2);
+        h(u, v) = 1 - exp(-1 * distance^2 / (2 * d^2));
+    end
+end
+s = s .* h;  % Filtering
+
+% After filtering
+img = uint8(real(ifft2(ifftshift(s))));           % Change back to a picture
+subplot(234), imshow(img);
+title('After filtering');
+
+% Flourier transformation && Frequency shift
+s = fftshift(fft2(img));
+subplot(235), imshow(abs(s), []);
+title('Flourier & Shift');
+
+% Get log
+subplot(236), imshow(log(abs(s)), []);
+title('Getlog');
+
+figure, surf(1:m, 1:n, h);
+axis([1 m 1 n 0 1]);
--- test.m
+++ test.m
@@ -1,27 +1,25 @@
-I=imread('lena.bmp');
-subplot(221),imshow(I);
-xlabel('a原图像');
-s=fftshift(fft2(I));
-subplot(222),imshow(log(abs(s)),[]);
-xlabel('b图像傅里叶变换取对数所得频谱');
-[a,b]=size(s);
-a0=round(a/2);
-b0=round(b/2);
-d=10;
-for i=1:a
-    for j=1:b
-        distance=sqrt((i-a0)^2+(j-b0)^2);
-        if distance<=d
-            h=1;
-        else
-            h=0;
-        end;
-        s(i,j)=h*s(i,j);
-    end;
-end;
-F3=log(abs(s));%对傅里叶变换结果取绝对值，然后取对数??
-subplot(223),imshow(F3,'InitialMagnification','fit');
-xlabel('c滤波后的傅里叶变换图像');
-s=uint8(real(ifft2(ifftshift(s))));
-subplot(224),imshow(s);
-xlabel('d理想低通滤波图像');
+A=imread('lena24.bmp'); %????
+A=rgb2gray(A);
+[m,n]=size(A);
+figure(4)
+subplot(2,2,1);imshow(A);title('????');
+subplot(2,2,2);imhist(A);title('???????');% ??????
+k0=30;
+k1=300;
+k=1:256;
+yr=256*(1-(1/(256-k0).^2)*(k-256).^2);
+yg=256*(1-(1/((k1-k0)/2).^2)*(k-k0-(k1-k0)/2).^2);
+yb=256*(1-(1/(k1-k0).^2)*(k-k0).^2)/2; %?2????????
+yr(1:k0)=0;yg(1:k0)=0;yb(1:k0)=0;
+subplot(2,2,3);plot(k,yr,'r',k,yg,'g',k,yb,'b');title('???????');
+for j=1:n
+for i=1:m
+if(A(i,j)==0) 
+A(i,j)=1; 
+end
+CC(i,j,1)=uint8(yr(A(i,j))); 
+CC(i,j,2)=uint8(yg(A(i,j)));
+CC(i,j,3)=uint8(yb(A(i,j))); 
+end
+end
+subplot(2,2,4);imshow(CC);title('?????');
