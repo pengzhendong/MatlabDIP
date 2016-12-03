73ebdce1e2f3580481759a93affe79d687a91a9f
pengzhendong <275331498@qq.com>
2016-12-03 20:24:31 +0800

    Finish lab3

--- lab3/get_marr.m
+++ lab3/get_marr.m
@@ -14,19 +14,9 @@ if mod(len, 2) == 0
     len = len + 1;
 end
 
-% Caculate the value of LoG
-xa = -5*sigma:0.1:5*sigma;
-ya = xa;
-[x, y] = meshgrid(xa, ya);
-del = (x.^2 + y.^2 - 2*sigma^2) / sigma^4;
-gauss = exp(-1 * (x.^2 + y.^2) / (2*sigma^2));
-log = del .* gauss;
-figure, plot3(x, y, -log);
-title('LoG');
-
 set (gcf,'Position',[300,200,800,300])
 % Original picture
-subplot(121), imshow(grey);
+figure(1), subplot(131), imshow(grey);
 title('Original picture');
 
 % Caculate the  templet
@@ -43,7 +33,29 @@ end
 
 % Edge detection
 marr = conv2(im2double(grey), templet);
-subplot(122), imshow(marr);
+figure(1), subplot(132), imshow(marr);
 title('Original picture');
 
+% Threshold processing
+threshold = max(max(marr)) * 0.18;
+for i = 1:m
+    for j = 1:n
+        if marr(i, j) >= threshold
+            marr(i, j) = 1;
+        else
+            marr(i, j) = 0;
+        end
+    end
+end
+subplot(133), imshow(marr);
+title('Threshold processing');
 
+% Caculate the value of LoG
+xa = -5*sigma:0.1:5*sigma;
+ya = xa;
+[x, y] = meshgrid(xa, ya);
+del = (x.^2 + y.^2 - 2*sigma^2) / sigma^4;
+gauss = exp(-1 * (x.^2 + y.^2) / (2*sigma^2));
+log = del .* gauss;
+figure(2), plot3(x, y, -log);
+title('LoG');
